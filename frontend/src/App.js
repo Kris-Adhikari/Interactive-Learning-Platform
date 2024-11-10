8df1f733144e16c1590a6bb36b386f38e2b7d793
Kris <167502686+Kris-Adhikari@users.noreply.github.com>
2024-11-10 03:35:04 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import VideoPlayer from './components/VideoPlayer';
@@ -12,13 +13,29 @@ import './App.css';
 function App() {
   const [points, setPoints] = useState(0);
   const [currentTopic, setCurrentTopic] = useState("critical-thinking");
+  const [progress, setProgress] = useState({
+    "critical-thinking": { quizCompleted: false },
+    "networking-marginalized": { quizCompleted: false }
+  });
 
   useEffect(() => {
     setPoints(0); 
+    const savedProgress = JSON.parse(localStorage.getItem('userProgress')) || progress;
+    setProgress(savedProgress);
   }, []);
 
-  const handleQuizCompletion = () => {
-    setPoints(points + 10); // increase points by 10 for each completed quiz
+  const handleQuizCompletion = (topic) => {
+    setPoints(points + 10); // Increase points by 10 for each completed quiz
+    markQuizAsCompleted(topic);
+  };
+
+  const markQuizAsCompleted = (topic) => {
+    const updatedProgress = {
+      ...progress,
+      [topic]: { quizCompleted: true }
+    };
+    setProgress(updatedProgress);
+    localStorage.setItem('userProgress', JSON.stringify(updatedProgress));
   };
 
   const handleTopicChange = (topic) => {
@@ -34,13 +51,13 @@ function App() {
           <main className="main-content">
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/library" element={<LibraryPage />} /> {}
+              <Route path="/library" element={<LibraryPage />} />
               <Route path="/topic/:topic" element={<VideoPlayer videoUrl="/videos/WhatIsCriticalThinking.mp4" />} />
-              <Route path="/quiz/:topic" element={<QuizPage onQuizComplete={handleQuizCompletion} />} />
+              <Route path="/quiz/:topic" element={<QuizPage onQuizComplete={(topic) => handleQuizCompletion(topic)} />} />
             </Routes>
           </main>
         </div>
-        <Chatbot /> {}
+        <Chatbot />
       </div>
     </Router>
   );
