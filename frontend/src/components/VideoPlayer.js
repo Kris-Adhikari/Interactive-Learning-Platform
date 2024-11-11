2213aba9814721b3b3eea6161a69c5ea122dd6b3
Kris <167502686+Kris-Adhikari@users.noreply.github.com>
2024-11-11 08:57:43 -0700

    Update VideoPlayer.js

--- frontend/src/components/VideoPlayer.js
+++ frontend/src/components/VideoPlayer.js
@@ -1,33 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import './VideoPlayer.css';
 
-function VideoPlayer({ videoUrl, currentTopic }) {
+function VideoPlayer({ videoUrl }) {
   const videoRef = useRef(null);
-
-  useEffect(() => {
-    const video = videoRef.current;
-
-    const handleTimeUpdate = () => {
-      if (video && video.currentTime >= video.duration - 2) {
-        console.log("Video detected as completed");
-        const progress = JSON.parse(localStorage.getItem('userProgress')) || {};
-        progress[currentTopic] = { ...progress[currentTopic], videoWatched: true };
-        localStorage.setItem('userProgress', JSON.stringify(progress));
-        
-        video.removeEventListener('timeupdate', handleTimeUpdate);
-      }
-    };
-
-    if (video) {
-      video.addEventListener('timeupdate', handleTimeUpdate);
-    }
-
-    return () => {
-      if (video) {
-        video.removeEventListener('timeupdate', handleTimeUpdate);
-      }
-    };
-  }, [currentTopic]);
 
   return (
     <div className="video-player">
